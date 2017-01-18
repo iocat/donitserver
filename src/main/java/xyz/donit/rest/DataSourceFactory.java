b01713673d912154e7b8d6ec937a044e3eaaf82f
Thanh Ngo <felix.infinite@gmail.com>
2017-01-18 02:09:13 -0500

    hardcode the database

--- src/main/java/xyz/donit/rest/DataSourceFactory.java
+++ src/main/java/xyz/donit/rest/DataSourceFactory.java
@@ -3,29 +3,24 @@ package xyz.donit.rest;
 import org.apache.commons.dbcp2.BasicDataSource;
 import org.glassfish.hk2.api.Factory;
 
-import javax.sql.DataSource;
-import java.net.URI;
-import java.net.URISyntaxException;
-
 public class DataSourceFactory implements Factory<BasicDataSource> {
     private static BasicDataSource connectionPool;
     private static final String JDBC_DRIVER = "org.postgresql.Driver";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    private static final String HOST = "donit-database";
+    private static final String PORT = "26257";
+    private static final String DBNAME = "donit";
     public DataSourceFactory(){
         try {
             Class.forName(JDBC_DRIVER);
             this.connectionPool = new BasicDataSource();
-            URI dbUri = new URI(System.getenv("DONITDB"));
-            String dbUrl = "jdbc:postgresql://" + dbUri.getHost()+":"+dbUri.getPort() + dbUri.getPath();
-            if (dbUri.getUserInfo() != null) {
-                connectionPool.setUsername(dbUri.getUserInfo().split(":")[0]);
-                connectionPool.setPassword(dbUri.getUserInfo().split(":")[1]);
-            }
+            String dbUrl = "jdbc:postgresql://" + HOST+":"+PORT + "/"+ DBNAME;
+            connectionPool.setUsername(USERNAME);
             connectionPool.setDriverClassName("org.postgresql.Driver");
             connectionPool.setUrl(dbUrl);
             connectionPool.setInitialSize(1);
             connectionPool.setDefaultAutoCommit(true);
-        }catch (URISyntaxException e){
-            e.printStackTrace();
         }catch (ClassNotFoundException e){
             e.printStackTrace();
         }
